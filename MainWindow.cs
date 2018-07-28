0e8301f71a0271793b16b666927c0456581fc0a8
Guillaume Lacasse <glacasse@gmail.com>
2018-07-28 15:40:06 -0400

    Raw mouse

--- MainWindow.cs
+++ MainWindow.cs
@@ -12,6 +12,10 @@ namespace RawMouseInputDevice
 {
     public partial class MainWindow : Form, IMessageFilter
     {
+        private int previousX = 0;
+
+        private int previousY = 0;
+
         private RawMouseHandler mouseHandler;
 
         //  Initialization
@@ -33,16 +37,13 @@ namespace RawMouseInputDevice
             int y;
             mouseHandler.GetMouseMovement(message, out x, out y);
 
-            int previousX = 0;
-            int previousY = 0;
-
             if (x != previousX || y != previousY)
             {
-                System.Diagnostics.Debug.WriteLine("Position : " + x + ", " + y);
+                System.Diagnostics.Debug.WriteLine(x + ", " + y);
             }
 
-            previousX = x;
-            previousY = y;
+            this.previousX = x;
+            this.previousY = y;
 
             return false;
         }
